6c1cce6230838112d50f6d3b2c7fd94a1afb0eec
Cristhian Diaz <cristhiancamilodiazromero@gmail.com>
2024-03-28 13:35:16 -0500

    Update possible classes used in the proyect
    I updated the proyect so it gets new classes, yet there are not with the specified attributes

---
+++ UberWorkSpace/UberUsers/src/co/edu/unbosque/model/Passanger.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.model;
+
+public class Passanger {
+
+	private String name;
+	private long documentId;
+
+}
---
+++ UberWorkSpace/UberUsers/src/co/edu/unbosque/model/User.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.model;
+
+public class User {
+
+	private String user;
+	private String password;
+
+}
